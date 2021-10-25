611e86cf999c5dbb1ad2cb5e792fae99a6640a32
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2021-10-25 11:24:35 +0700

    add sw.js

---
+++ src/scripts/sw.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-restricted-globals */
+self.addEventListener('install', (event) => {
+  console.log('installing service worker')
+  // do something
+})
+
+self.addEventListener('activate', (event) => {
+  console.log('activating service worker')
+})
+
+self.addEventListener('fetch', (event) => {
+  console.log(event.request)
+
+  event.respondWith(fetch(event.request))
+})
